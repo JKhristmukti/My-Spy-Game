4ebe3d441128eab5584c571239a4515108784585
Jason.A.Khristmukti <64148391+JKhristmukti@users.noreply.github.com>
2020-09-02 22:46:02 +0530

    Part-2 of the game

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var spy;
 var spyimg;
+var coinimg;
 var ground;
 
 var guard = [];
+var Ob = [];
 var guard1;
 var guard2;
 var guard3;
@@ -16,7 +18,9 @@ var g1,g2,g3,g4,g5,g6,g7;
 var bgimg;
 
 function preload(){
+    bgimg = loadImage("BG.jpg");
     spyimg = loadImage("spy1.jpg");
+    coinimg = loadImage("coin.jpg");
     guard1 = loadImage("guards-1.jpg");
     guard2 = loadImage("guards-2.jpg");
     guard3 = loadImage("guards-3.jpg");
@@ -38,23 +42,31 @@ function setup() {
 }
 
 function draw() {
-  background("white");
+  background(bgimg);
 
-  spy.collide(ground);
+  spawnCoins();
 
   if(ground.x<0){
     ground.x = ground.width/2;
   }
 
-  if(keyCode === 32 && spy.y>=300){
-      spy.velocity.y = -7;
+  if(spy.x<50){
+      spy.x = 50; 
   }
 
-  spy.y = spy.y + 0.8;
-  
-  console.log(spy.y);
+  if(spy.y>343.05){
+      spy.y = 342;
+  }
+
+  spy.velocity.y = spy.velocity.y + 0.8;
 
   spawnObstacles();
+  spawnobstacles2();
+
+  spawnEnemies();
+
+  spy.collide(ground);
+  spy.collide(Ob);
 
   drawSprites();
 }
@@ -63,9 +75,70 @@ function spawnObstacles(){
     if(frameCount%100===0){
         var r = random(0,100);
         var r2 = random(0,75);
-        var obs = createSprite(800,350,20,50);
-        obs.velocity.x = -3;
-        obs.width = r;
-        obs.height = r2;
+          var obs = createSprite(800,350,20,50);
+          obs.velocity.x = -3;
+          obs.y = random(200,280);
+          obs.width = r;
+          obs.height = r2;
+          Ob.push(obs);
+    }
+}
+
+function spawnobstacles2(){
+  if(frameCount%100===0){
+    var r = random(0,100);
+    var r2 = random(0,75);
+      var obs = createSprite(800,350,20,50);
+      obs.velocity.x = -3;
+      obs.y = random(320,400);
+      obs.width = r;
+      obs.height = r2;
+      Ob.push(obs);
+  }
+}
+
+function spawnCoins(){
+    if(frameCount%300===0){
+        var r = random(150,300);
+        var coin = createSprite(800,200,20,20);
+        coin.y = r;
+        coin.velocity.x = -2;
+        coin.addImage(coinimg);
+        coin.scale = 0.3;
+    }
+}
+
+function spawnEnemies(){
+  if(frameCount%220===0){
+    var r = random(2,4);
+    var enemy = createSprite(400,400,50,50);
+    enemy.x = random(600,800);
+    enemy.y = random(250,350);
+    enemy.velocity.x = -r;
+    var rand = Math.round(random(1,7));
+
+    switch(rand){
+        case 1: enemy.addImage(guard1);
+              break;
+        case 2: enemy.addImage(guard2);
+              break;
+        case 3: enemy.addImage(guard1);
+              break;
+        case 4: enemy.addImage(guard1);
+              break;
+        case 5: enemy.addImage(guard1);
+              break;
+        case 6: enemy.addImage(guard1);
+              break;
+        case 7: enemy.addImage(guard1);
+              break;
+    }
+    enemy.scale = 0.3;
+  }
+}
+
+function keyPressed(){
+    if(keyCode===32 && spy.y>200){
+        spy.velocity.y = -15;
     }
 }
